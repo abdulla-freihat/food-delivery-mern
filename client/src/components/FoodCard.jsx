c5a79fed8fc687b794c4328a1e948c3138db5f49
abdulla freihat <abdulla_freihat@hotmail.com>
2024-05-06 22:26:38 -0400

    add cart  functionality using context api inside the food card to store the cart items

--- client/src/components/FoodCard.jsx
+++ client/src/components/FoodCard.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useState } from 'react'
+import { useContext } from 'react'
+ 
+import {StoreContext} from '../context/StoreContext';
 
 const FoodCard = ({item}) => {
 
 
     const [itemCount , setItemCount] = useState(0)
+
+
+    const {cartItems , addToCart , removeFromCart} = useContext(StoreContext)
+
+   
   return (
     <div  className='shadow rounded-lg  flex flex-col  gap-3 mt-8 animate-fade '> 
 
@@ -13,13 +21,13 @@ const FoodCard = ({item}) => {
 
      <img src={item.image}   alt={item.name} className='bg-cover rounded-t-lg ' />
         
-         {!itemCount   
-             ? <img  onClick={()=>setItemCount((prev)=>prev+1)} src={assets.add_icon_white}  className='bg-white rounded-full shadow  text-center w-8 h-8 absolute  bottom-[5%] right-[5%] cursor-pointer'/>
+         {!cartItems[item._id]  
+             ? <img  onClick={()=>addToCart(item._id)} src={assets.add_icon_white}  className='bg-white rounded-full shadow  text-center w-8 h-8 absolute  bottom-[5%] right-[5%] cursor-pointer'/>
              : <div  className='  bg-white p-1 rounded-full absolute  bottom-[5%] right-[5%] cursor-pointer flex items-center gap-2'>
-             <img  onClick={()=>setItemCount((prev)=>prev-1)} src={assets.remove_icon_red} className='w-8 h-8' />
-                 <p >{itemCount}</p>
+             <img   onClick={()=>removeFromCart(item._id)} src={assets.remove_icon_red} className='w-8 h-8' />
+                 <p >{cartItems[item._id]}</p>
 
-                 <img  onClick={()=>setItemCount((prev)=>prev+1)} src={assets.add_icon_green} className='w-8 h-8' />
+                 <img  onClick={()=>addToCart(item._id)} src={assets.add_icon_green} className='w-8 h-8' />
 
              </div>
          }              
