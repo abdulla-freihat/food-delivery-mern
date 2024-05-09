9002aba0dc0c397ebe840bcec3e190bcc6f49797
abdulla freihat <abdulla_freihat@hotmail.com>
2024-05-09 02:00:57 -0400

    edit app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/Navbar"
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/cart/Cart";
 import StoreContextProvider from "./context/StoreContext.jsx";
@@ -9,37 +9,33 @@ import AuthPopup from "./components/AuthPopup.jsx";
 import Page404 from "./pages/Page404.jsx";
 
 function App() {
-
-
-  const [showAuthForm , setShowAuthForm] = useState(false);
+  const [showAuthForm, setShowAuthForm] = useState(false);
 
   return (
     <>
       <BrowserRouter>
+        <StoreContextProvider>
+          {showAuthForm ? (
+            <AuthPopup setShowAuthForm={setShowAuthForm} />
+          ) : (
+            <></>
+          )}
 
-         
+          <Navbar setShowAuthForm={setShowAuthForm} />
 
-      <StoreContextProvider>
-        
-          {showAuthForm ? <AuthPopup setShowAuthForm={setShowAuthForm} /> : <></>}
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-         <Navbar setShowAuthForm = {setShowAuthForm} />
+            <Route path="/cart" element={<Cart />} />
 
-            <Routes>
-               <Route  path ='/'  element={<Home />} />
-              
-               <Route  path ='/cart'  element={<Cart />} />
-               <Route  path ='*'  element={<Page404 />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
 
-         
-            </Routes>
-
-              <Footer />
-            </StoreContextProvider>
+          <Footer />
+        </StoreContextProvider>
       </BrowserRouter>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
