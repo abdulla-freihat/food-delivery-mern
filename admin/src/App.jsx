5c88ea8df593da219c6e4c3e86a28b0da8624034
abdulla freihat <abdulla_freihat@hotmail.com>
2024-05-13 19:21:09 -0400

    install dependencies and add the assessts admin folder

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-     <p className='text-red-600'>admin</p>
-    </>
+    <div>App</div>
   )
 }
 
