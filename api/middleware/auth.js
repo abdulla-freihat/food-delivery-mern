0ecee594e45f12158e5acf25f34f456cee52b9fc
abdulla freihat <abdulla_freihat@hotmail.com>
2024-05-20 23:41:59 -0400

    add auth middle ware token

---
+++ api/middleware/auth.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+
+ const authMiddleware =async (req, res ,next)=>{
+
+     const {token} = req.headers;
+
+      if(!token){
+             return res.json({success:false , message:'Unauthorized'})
+      }
+
+
+      try{
+
+          const token_decode = jwt.verify(token , process.env.JWT_SECRET);
+
+          req.body.userId = token_decode.id;
+
+          next();
+
+      }catch(err){
+        return res.status(500).json({success:false , message:'Error'})
+
+         
+      }
+     
+     
+ }
+
+
+ export default authMiddleware;
