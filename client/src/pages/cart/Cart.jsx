642bee2b580d18d8671a0312d9ea113222622e26
abdulla freihat <abdulla_freihat@hotmail.com>
2024-06-10 23:06:38 -0400

    add cart empty image functionality if the cart is empty

--- client/src/pages/cart/Cart.jsx
+++ client/src/pages/cart/Cart.jsx
@@ -1,18 +1,44 @@
 import React ,{useContext} from 'react'
 import CartItems from './CartItems'
+import { assets } from '../../assets/assets.js'
 
 import {StoreContext} from "../../context/StoreContext.jsx"
 import CartOrderDetails from './CartOrderDetails.jsx'
+import { Link } from 'react-router-dom' 
 
 const Cart = () => {
 
   const {food_list  , cartItems , removeFromCart , getCartTotalAmount , url} = useContext(StoreContext);
 
-  return (
-    <div className='p-5 '>
-        <CartItems food_list={food_list} cartItems={cartItems}  removeFromCart={removeFromCart} url={url}  />
+     
+   const cartEmpty = ()=>{
 
-        <CartOrderDetails  getCartTotalAmount ={getCartTotalAmount} />
+    return Object.keys(cartItems).length===0 || Object.values(cartItems).every(count => count <= 0)
+   }
+
+  return (
+    <div className='p-5 h-screen '>
+
+    {cartEmpty() ?(
+
+      <div className='flex flex-col justify-center items-center p-2 animate-fade'>
+      <img src={assets.cart_empty_1}   alt=''/>
+         
+         
+          <Link to='/' className='bg-orange-500 text-white px-8 py-2 rounded-md '>Go Home</Link>
+      
+        
+      </div>
+    )  :(
+      <>
+      <CartItems food_list={food_list} cartItems={cartItems}  removeFromCart={removeFromCart} url={url}  />
+
+      <CartOrderDetails  getCartTotalAmount ={getCartTotalAmount} />
+</>
+
+       
+    )}
+
 
        
     </div>
